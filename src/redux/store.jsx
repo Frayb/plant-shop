19a20a6c93e871cb475c8c7b98d005f20fb88f6a
Fraol Bekele <105886413+Frayb@users.noreply.github.com>
2025-04-14 10:28:00 -0500

    cartreduce Update store.jsx

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import cartReducer from "./slices/cartSlice.jsx";
+import cartReducer from "../redux/slices/CartSlice";
 
 export const store = configureStore({
   reducer: {
